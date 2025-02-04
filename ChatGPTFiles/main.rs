7a6c569870e8733ca6e7f3b798f54bc79d557594
CoderSales <stephenobrien1@gmail.com>
2025-02-04 00:47:26 +0000

    Add 2 files to ChatGPT as references Build project
    use commands
    
    cargo build
    
    to build project
    
    and
    
    cargo run
    
    to run
    
    Note1
    Add test wav file
    to
    same directory
    as
    main rs
    file
    
    Edit
    
    file name to
    test wav
    in
    
    main rs
    
    cd into
    folder
    main
    src
    
    to use
    cargo build
    
    cargo run

---
+++ ChatGPTFiles/main.rs
@@ -0,0 +1,20 @@
+use hound;
+
+fn main() {
+    let filename = "test.wav"; // Change this to your WAV file
+    let reader = match hound::WavReader::open(filename) {
+        Ok(r) => r,
+        Err(e) => {
+            eprintln!("Error loading file: {}", e);
+            return;
+        }
+    };
+
+    let spec = reader.spec();
+    println!("Sample Rate: {}", spec.sample_rate);
+    println!("Bits per Sample: {}", spec.bits_per_sample);
+    println!("Channels: {}", spec.channels);
+
+    let samples: Vec<i16> = reader.into_samples::<i16>().filter_map(Result::ok).collect();
+    println!("Loaded {} samples", samples.len());
+}
