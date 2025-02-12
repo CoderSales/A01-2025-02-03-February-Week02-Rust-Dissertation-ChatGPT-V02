3e704c07af0d3beacd59bc8fe9e623612201b317
CoderSales <stephenobrien1@gmail.com>
2025-02-12 12:31:48 +0000

    Modify audio rs
    Implement
    play recorded audio
    in
    AudioProcessor
    
    Reduce file from
    about
    88 lines
    to 34 lines

--- main/src/audio.rs
+++ main/src/audio.rs
@@ -1,8 +1,8 @@
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, Sink};
 use std::sync::{Arc, Mutex};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-
-const CHUNK_SIZE: usize = 256;
-const SAMPLE_RATE: f64 = 44100.0;
 
 pub struct AudioProcessor {
     pub waveform: Arc<Mutex<Vec<f64>>>,
@@ -13,76 +13,22 @@ pub struct AudioProcessor {
 
 impl AudioProcessor {
     pub fn new() -> Self {
-        let waveform = Arc::new(Mutex::new(vec![0.0; CHUNK_SIZE]));
-        let fft_result = Arc::new(Mutex::new(vec![0.0; CHUNK_SIZE / 2]));
-        let dominant_frequency = Arc::new(Mutex::new(0.0));
-
         Self {
-            waveform,
-            fft_result,
-            dominant_frequency,
+            waveform: Arc::new(Mutex::new(vec![0.0; 256])),
+            fft_result: Arc::new(Mutex::new(vec![0.0; 128])),
+            dominant_frequency: Arc::new(Mutex::new(0.0)),
             stream: None,
         }
     }
 
-    pub fn start_listening(&mut self) {
-        let waveform_clone = Arc::clone(&self.waveform);
-        let fft_result_clone = Arc::clone(&self.fft_result);
-        let dominant_frequency_clone = Arc::clone(&self.dominant_frequency);
+    pub fn play_recorded_audio(&self) {
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
 
-        let host = cpal::default_host();
+        let file = BufReader::new(File::open("recorded_audio.wav").expect("Audio file not found"));
+        let source = Decoder::new(file).unwrap();
 
-        // Select the **VB-Audio Virtual Cable** as the input device
-        let device = host.input_devices().unwrap()
-            .find(|d| d.name().unwrap_or_default().contains("CABLE Output")) // Adjust based on your OS
-            .expect("Virtual Audio Cable not found!");
-
-        let config = device.default_input_config().unwrap().into();
-
-        let stream = device.build_input_stream(
-            &config,
-            move |data: &[f32], _: &cpal::InputCallbackInfo| {
-                let mut waveform_data = waveform_clone.lock().unwrap();
-                waveform_data.clear();
-                waveform_data.extend(data.iter().map(|&s| s as f64));
-
-                let mut fft_data = fft_result_clone.lock().unwrap();
-                *fft_data = AudioProcessor::compute_fft(&waveform_data);
-
-                let mut dominant_freq = dominant_frequency_clone.lock().unwrap();
-                *dominant_freq = AudioProcessor::find_dominant_frequency(&fft_data);
-            },
-            |err| eprintln!("Stream error: {:?}", err),
-            None,
-        ).unwrap();
-
-        stream.play().unwrap();
-        self.stream = Some(stream);
-    }
-
-    pub fn stop_listening(&mut self) {
-        if let Some(stream) = self.stream.take() {
-            drop(stream);
-        }
-    }
-
-    fn compute_fft(samples: &[f64]) -> Vec<f64> {
-        let len = samples.len().next_power_of_two();
-        let mut planner = rustfft::FftPlanner::new();
-        let fft = planner.plan_fft_forward(len);
-        let mut buffer: Vec<rustfft::num_complex::Complex<f64>> = 
-            samples.iter().map(|&s| rustfft::num_complex::Complex::new(s, 0.0)).collect();
-        buffer.resize(len, rustfft::num_complex::Complex::new(0.0, 0.0));
-        fft.process(&mut buffer);
-        buffer.iter().take(len / 2).map(|c| c.norm()).collect()
-    }
-
-    fn find_dominant_frequency(fft_data: &[f64]) -> f64 {
-        let max_index = fft_data.iter()
-            .enumerate()
-            .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
-            .unwrap()
-            .0;
-        (max_index as f64) * (SAMPLE_RATE / CHUNK_SIZE as f64)
+        sink.append(source);
+        sink.sleep_until_end(); // Block until audio finishes playing
     }
 }
